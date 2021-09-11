a023b3f615bf8b67112197abe29d4622ba65557d
Carlos Vallejo <chvallej@gmail.com>
2021-09-11 15:03:48 -0500

    First version

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,17 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath=chvallej/api
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
-
+kubectl run apiprediction\
+    --generator=run-pod/v1\
+    --image=$dockerpath\
+    --port=80 --labels app=apiprediction
 # Step 3:
 # List kubernetes pods
-
+kubectl get pods
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward apiprediction 8000:80
--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,13 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
-
+dockerpath=chvallej/api
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker tag api $dockerpath
+docker login --username chvallej
 
 # Step 3:
 # Push image to a docker repository
+docker image push $dockerpath
